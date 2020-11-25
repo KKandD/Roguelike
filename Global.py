7ef1045c198f1f6ad3d8c6b05dfe5713ba43b446
Boner1975 <boneckipawel1975@gmail.com>
2020-11-25 14:49:00 +0100

    Add class Player to Global.py

--- Global.py
+++ Global.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 
 class Global_class:
 
@@ -8,3 +8,24 @@ class Global_class:
         file_dir = os.path.dirname(os.path.abspath(__file__))
         my_file = os.path.join(file_dir, file_name)
         return my_file
+
+class Player():
+
+    def __init__(self, hit_count, name, icon):
+        self.hit_count = hit_count
+        self.name = name
+        self.icon = icon
+        
+    def player_move(self):
+        pass
+
+    def fight_randomness(self):
+        hit_or_not = ['H', 'H', 'H', 'H', 'L']
+        random_result = random.choice(hit_or_not)
+        return random_result
+    
+    def pick_fish(self):
+        self.hit_count += 2
+
+    def print_player_parameters(self):
+        print(f'Player name {self.name}\nHit_count {self.hit_count}\nIcon {self.icon}' )
